3abede4f070be58ca0f967afff5e105a8d5ef905
Johannes Opper <xijo@pm.me>
2018-12-02 19:20:31 +0100

    Use Enum.sum

--- day01/part1.exs
+++ day01/part1.exs
@@ -3,5 +3,5 @@ File.read!("input.txt")
 |> String.split("\n")
 |> Enum.map(&Integer.parse/1)
 |> Enum.map(&(elem(&1, 0)))
-|> Enum.reduce(fn(value, acc) -> value + acc end)
+|> Enum.sum
 |> IO.inspect
